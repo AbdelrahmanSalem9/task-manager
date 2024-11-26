e1c3e9bcac6153314d10360a02b69d41b0ff9ff7
AbdelrahmanSalem9 <Abdelrahmansalem16@gmail.com>
2024-11-26 20:23:18 +0200

    create tasks endpoints

--- routes/Task.js
+++ routes/Task.js
@@ -1,28 +1,25 @@
 const router = require('express').Router();
+const validateTask = require('../util/TaskValidation.js');
+const taskController = require('../controller/TaskController.js');
+const filteringMiddleware = require('../util/TaskFilterMW.js');
+const sortingMiddleware = require('../util/TaskSortingMW.js');
 
 
+// Get Tasks applying filters
+router.get('/', filteringMiddleware, sortingMiddleware, taskController.getTasks);
 
-// Get all tasks sorted by priority/due date
-
-
-// Get {priority} tasks
-
-
-// Get {status} tasks
-
-
-// Get {Date} tasks
-
-// Get today due tasks
-
+router.get('/:title', taskController.getTask);
 
 // Create new Task
+router.post('/', validateTask, taskController.createNewTask);
 
 
 // Update Task
+router.put('/:title', taskController.updateTask);
 
 
 // Delete Task
+router.delete('/:title', taskController.deleteTask);
 
 
 module.exports = router
