88549e1dd27f4e49d45ba5eb3982dc07a84a517e
AbdelrahmanSalem9 <Abdelrahmansalem16@gmail.com>
2024-11-26 20:22:38 +0200

    remove task title uniquness

--- model/TaskModel.js
+++ model/TaskModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    title: { type: String, required: true, unique: true },
+    title: { type: String, required: true },
     description: { type: String },
     status: {
         type: String,
