b0b09dab8fa2dcfe9b84ea268f3fc0633427c046
AbdelrahmanSalem9 <Abdelrahmansalem16@gmail.com>
2024-11-26 21:04:02 +0200

    refactor the app file

--- app.js
+++ app.js
@@ -1,30 +1,48 @@
 const express = require('express');
-const app = express();
-require('dotenv').config()
-const userRouter = require('./routes/User.js');
 const mongoose = require('mongoose');
-const loginRouter = require('./routes/auth.js');
-const taskRouter = require('./routes/Task.js');
-const attachUserID = require('./util/attachUserID.js');
-const loggerMiddleware = require('./middlewares/loggerMW.js');
-const uri = process.env.DATABASE_URL;
-const port = process.env.PORT;
+require('dotenv').config();
 
+const app = express();
 
+// Import routes
+const userRouter = require('./routes/User');
+const loginRouter = require('./routes/auth');
+const taskRouter = require('./routes/Task');
+
+// Middleware
+const attachUserID = require('./util/attachUserID');
+const loggerMiddleware = require('./middlewares/loggerMW');
+
+// Get environment variables
+const { DATABASE_URL, PORT } = process.env;
+
+// Middleware setup
 app.use(express.json());
 app.use(loggerMiddleware);
+
+// Routes setup
 app.use('/user', userRouter);
 app.use('/login', loginRouter);
 app.use('/tasks', attachUserID, taskRouter);
-mongoose.connect(uri).then(() => {
-    console.log("Database connection is established.....");
-}).catch((err) => {
-    console.log(err);
-    console.log("Exit Process");
-    process.exit(1);
-})
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
 
+// Database connection
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("Database connection established...");
+    } catch (err) {
+        console.error("Error connecting to the database", err);
+        process.exit(1); // Exit the process with failure code
+    }
+};
 
+// Start server and connect to DB
+const startServer = async () => {
+    await connectToDatabase();
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+};
+
+// Run the application
+startServer();
